ce695ff73c44e07012844f28153a248d58f3e695
Sean Crawford <Sheath51@gmail.com>
2019-03-07 15:25:44 -0700

    Updated some time domain functions.

--- lib/timeprocessing.c
+++ lib/timeprocessing.c
@@ -2,27 +2,40 @@
 #include "timeprocessing.h"
 #include "globals.h"
 
-double reorderData(){
+double reorderData();
 
+/***********************************************/
+/* windowData will accept a a struture with    */
+/* an array of ADC values and will multiply    */
+/* each value with a corresponding window      */
+/* weight found in the globals.c file. The     */
+/* struct with the data array will  then be    */
+/* returned to the main function.              */
+/***********************************************/
+struct signal windowData(struct signal data){
+    for(uint32_t i=0; i<data.length; i++)
+        data.values[i] = data.values[i] * window_weights[i];
+    return data;
 }
 
-/***********************************************/
-/* windowData will accept a point to an array  */
-/* of doubles and will multiply each value     */
-/* with a corresponding window weight found    */
-/* in the globals.c file. The pointer to the   */
-/* data array will now hold the windowed data. */
-/***********************************************/
-void windowData(float *data[]){
-    for(uint16_t i=0; i<sizeof(data); i++){
-        data[i] = data[i] * window_weights[i];
+float antiAliasFilter(){
+    float y = 0; //value for y queue
+    for(uint32_t i = 0; i < FIR_COEF_COUNT; i++) // Iterates through the size of the x queue.
+    {
+        y += queue_readElementAt(&xQueue, FIR_COEF_COUNT - i - OFFSET)*firCoefficients[i]; // Convolves x queue with the filter coefficients.
     }
+    queue_overwritePush(&yQueue, y); // Add the computed value to the y queue.
+    return y; //Return the value as a double.
 }
 
-double antiAliasFilter(){
-
-}
-
+static uint16_t firDecimationCount = 0;
 double decimateData(){
-
+    if (firDecimationCount == DECIMATION_FACTOR - 1)
+    {
+        antiAliasFilter(); // Run antialias filter.
+        firDecimationCount = 0; // Reset decimation count.
+        return true; // Ran the filter.
+    }
+    firDecimationCount++; // Increment the decimation count each time the filter fuction is called.
+    return false; // Didn't run the filter.
 }
