4fcbe7c1ad7997d60f22f565e2599978ad9d293c
Jacob Holtom <jacob@holtom.me>
2019-03-11 09:19:26 -0600

    Fixed typing issue

--- lib/timeprocessing.c
+++ lib/timeprocessing.c
@@ -1,6 +1,5 @@
 #include "timeprocessing.h"
 
-<<<<<<< HEAD
 const char ADC1_GPIO[] = {9,25,10,22,27,17,18,15,14,24};
 const char ADC2_GPIO[] = {20,26,16,19,13,12,7,8,11,21};
 
@@ -12,7 +11,7 @@ void GetV2(uint32_t values[], uint32_t N, float *M){
     /* Reset all variables to zero and grab */
     /* current value in a signed int.       */
     for(uint32_t i=0; i<N; i++){
-        val = int(values[i]);
+        val = values[i];
         SignalZero = 0;
         tmp = 0;
         shift = 0;
@@ -48,7 +47,7 @@ void GetV1(uint32_t values[], uint32_t N, float *M2){
     /* Reset all variables to zero and grab */
     /* current value in a signed int.       */
     for(uint32_t i=0; i<N; i++){
-        val = int(values[i]);
+        val = values[i];
         SignalZero = 0;
         tmp = 0;
         shift = 0;
@@ -119,11 +118,13 @@ struct signal windowData(struct signal data){
 
 float antiAliasFilter(){
     float y = 0; //value for y queue
+    /* This needs to be rethought
     for(uint32_t i = 0; i < FIR_COEF_COUNT; i++) // Iterates through the size of the x queue.
     {
         y += queue_readElementAt(&xQueue, FIR_COEF_COUNT - i - OFFSET)*firCoefficients[i]; // Convolves x queue with the filter coefficients.
     }
     queue_overwritePush(&yQueue, y); // Add the computed value to the y queue.
+    */
     return y; //Return the value as a double.
 }
 
@@ -141,7 +142,6 @@ int decimateData(){
     */
     return 1;
 }
-/*
 void testCode(struct signal data){
     /* Call whichever function is under test */
     data = reorderData(data);
@@ -158,4 +158,3 @@ void testCode(struct signal data){
 
     fclose(dataOut);
 }
-*/
